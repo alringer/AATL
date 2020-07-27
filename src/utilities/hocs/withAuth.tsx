e4fb7ccd0faaa2ccf61cdf235a91e15abd106438
Yoon Chang <yoonsees@gmail.com>
2020-07-27 15:16:24 -0700

    - Fix Keycloak type file not returning genetric type for enhanced components

--- src/utilities/hocs/withAuth.tsx
+++ src/utilities/hocs/withAuth.tsx
@@ -14,68 +14,71 @@ export interface IWithAuthInjectedProps {
 }
 
 const withAuth = <P extends IWithAuthInjectedProps>(WrappedComponent: React.ComponentType<P>) => {
-    const KeycloakComponent: React.ComponentType<Subtract<P, IWithAuthInjectedProps>> = withKeycloak(
-        (props: ReactKeycloakInjectedProps & P) => {
-            const { keycloak, isServer, keycloakInitialized, ...passProps } = props
-            const handleLogin = () => {
-                if (keycloak) {
-                    keycloak.login()
-                } else {
-                    console.log('Keycloak not available')
-                }
+    // : React.ComponentType<Subtract<P, IWithAuthInjectedProps>>
+    const KeycloakComponent = withKeycloak((props: ReactKeycloakInjectedProps & P) => {
+        const { keycloak, isServer, keycloakInitialized, ...passProps } = props
+        const handleLogin = () => {
+            if (keycloak) {
+                keycloak.login()
+            } else {
+                console.log('Keycloak not available')
             }
-            const handleLogout = () => {
-                if (keycloak) {
-                    keycloak.logout()
-                } else {
-                    console.log('Keycloak not available')
-                }
-            }
-            const handleSignUp = () => {
-                if (keycloak) {
-                    keycloak.register()
-                } else {
-                    console.log('Keycloak not available')
-                }
-            }
-            const authenticatedAction = (callback: () => void) => {
-                if (keycloak && keycloak.authenticated) {
-                    callback()
-                } else {
-                    keycloak.login()
-                }
-            }
-            const getToken = () => {
-                if (keycloak && keycloak.token) {
-                    return keycloak.token
-                } else {
-                    return ''
-                }
-            }
-            const getTokenConfig = () => {
-                if (keycloak && keycloak.token) {
-                    return `Bearer ${keycloak.token}`
-                } else {
-                    return ''
-                }
-            }
-
-            return (
-                <WrappedComponent
-                    {...((passProps as unknown) as P)}
-                    keycloakLogin={handleLogin}
-                    keycloakLogout={handleLogout}
-                    keycloakSignUp={handleSignUp}
-                    authenticatedAction={authenticatedAction}
-                    getTokenConfig={getTokenConfig}
-                    getToken={getToken}
-                    authenticated={keycloak.authenticated}
-                    token={keycloak.token}
-                />
-            )
         }
-    )
-    return KeycloakComponent
+        const handleLogout = () => {
+            if (keycloak) {
+                keycloak.logout()
+            } else {
+                console.log('Keycloak not available')
+            }
+        }
+        const handleSignUp = () => {
+            if (keycloak) {
+                keycloak.register()
+            } else {
+                console.log('Keycloak not available')
+            }
+        }
+        const authenticatedAction = (callback: () => void) => {
+            if (keycloak && keycloak.authenticated) {
+                callback()
+            } else {
+                keycloak.login()
+            }
+        }
+        const getToken = () => {
+            if (keycloak && keycloak.token) {
+                return keycloak.token
+            } else {
+                return ''
+            }
+        }
+        const getTokenConfig = () => {
+            if (keycloak && keycloak.token) {
+                return `Bearer ${keycloak.token}`
+            } else {
+                return ''
+            }
+        }
+
+        return (
+            <WrappedComponent
+                {...((passProps as unknown) as P)}
+                keycloakLogin={handleLogin}
+                keycloakLogout={handleLogout}
+                keycloakSignUp={handleSignUp}
+                authenticatedAction={authenticatedAction}
+                getTokenConfig={getTokenConfig}
+                getToken={getToken}
+                authenticated={keycloak.authenticated}
+                token={keycloak.token}
+            />
+        )
+    })
+
+    const EnhancedComponent: React.ComponentType<Subtract<P, IWithAuthInjectedProps>> = () => {
+        return <KeycloakComponent />
+    }
+    return EnhancedComponent
 }
 
 export default withAuth
