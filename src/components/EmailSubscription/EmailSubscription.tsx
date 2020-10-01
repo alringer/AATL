a9139b253430f054546c5f70467a4315cadfb3c0
Yoon Chang <ychang@seamgen.com>
2020-09-30 21:42:15 -0700

    - Add regex pattern to email subscription component

--- src/components/EmailSubscription/EmailSubscription.tsx
+++ src/components/EmailSubscription/EmailSubscription.tsx
@@ -27,7 +27,10 @@ const EmailSubscription = () => {
 
     const handleSubscribe = () => {
         // TODO: Add more validations to check the validity of the input email
-        if (email !== '') {
+        const pattern = new RegExp(
+            /^(("[\w-\s]+")|([\w-]+(?:\.[\w-]+)*)|("[\w-\s]+")([\w-]+(?:\.[\w-]+)*))(@((?:[\w-]+\.)*\w[\w-]{0,66})\.([a-z]{2,6}(?:\.[a-z]{2})?)$)|(@\[?((25[0-5]\.|2[0-4][0-9]\.|1[0-9]{2}\.|[0-9]{1,2}\.))((25[0-5]|2[0-4][0-9]|1[0-9]{2}|[0-9]{1,2})\.){2}(25[0-5]|2[0-4][0-9]|1[0-9]{2}|[0-9]{1,2})\]?$)/i
+        )
+        if (pattern.test(email) && email !== '') {
             // TODO: Call email subscription API
             console.log('TODO: Call subscribe API with ', email)
             const config = {}
