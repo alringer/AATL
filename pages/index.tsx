4505f816717f9027761652b09138157f6ae826f1
Eric Hendrickson <ehendrickson@seamgen.com>
2020-11-18 14:04:01 -0800

    * Added token retrieval to front end

--- pages/index.tsx
+++ pages/index.tsx
@@ -3,7 +3,9 @@ import HomeNewRecommendations from 'components/Home/HomeNewRecommendations/HomeN
 import HomeBanner from 'components/HomeComponents/HomeBanner/HomeBanner'
 import MostRecommended from 'components/HomeComponents/MostRecommended/MostRecommended'
 import axios, { FETCH_HOME } from 'config/AxiosConfig'
+import { INSTAGRAM_CLIENT_ID, INSTAGRAM_CLIENT_SECRET, INSTAGRAM_REDIRECT_URI } from 'constants/InstagramConstants'
 import { KeycloakTokenParsed } from 'keycloak-js'
+import { useRouter } from 'next/router'
 import React from 'react'
 import { connect as reduxConnect } from 'react-redux'
 import { bindActionCreators } from 'redux'
@@ -26,10 +28,35 @@ interface IReduxProps {
     preferredLocation: ILocationInformation | null
 }
 
-interface IIndexProps extends IReduxProps, IWithAuthInjectedProps {}
+interface IIndexProps extends IReduxProps, IWithAuthInjectedProps { }
 
 const Index: React.FC<IIndexProps> = ({ preferredLocation }) => {
     const [homeData, setHomeData] = React.useState<IHomepage | null>(null)
+
+    const router = useRouter()
+    console.log(router)
+
+    if (router?.query['code']) {
+        const authorizationCode: string = router?.query['code'][0]
+        console.log(authorizationCode)
+        const formData = new FormData()
+        formData.append('client_id', INSTAGRAM_CLIENT_ID)
+        formData.append('client_secret', INSTAGRAM_CLIENT_SECRET)
+        formData.append('grant_type', 'authorization_code')
+        formData.append('code', authorizationCode)
+        formData.append('redirect_uri', INSTAGRAM_REDIRECT_URI)
+
+        const instagramToken = axios.post(
+            'https://api.instagram.com/oauth/access_token',
+            formData, {
+            headers: {
+                'Content-Type': 'multipart/form-data'
+            }
+        }).then(res => {
+            console.log(res['access_token'])
+            return res['access_token']
+        })
+    }
 
     React.useEffect(() => {
         if (preferredLocation) {
@@ -57,13 +84,13 @@ const Index: React.FC<IIndexProps> = ({ preferredLocation }) => {
                 }
                 initialPage={
                     homeData?.newRecommendations?.pageable?.pageNumber !== undefined &&
-                    homeData?.newRecommendations?.pageable?.pageNumber !== null
+                        homeData?.newRecommendations?.pageable?.pageNumber !== null
                         ? homeData.newRecommendations?.pageable?.pageNumber
                         : 0
                 }
                 initialPageSize={
                     homeData?.newRecommendations?.pageable?.pageSize !== undefined &&
-                    homeData?.newRecommendations?.pageable?.pageSize !== null
+                        homeData?.newRecommendations?.pageable?.pageSize !== null
                         ? homeData?.newRecommendations?.pageable?.pageSize
                         : 3
                 }
--- src/config/AxiosConfig.ts
+++ src/config/AxiosConfig.ts
@@ -28,9 +28,8 @@ export const REGISTER_VIEW = (id: number) => {
     return `/venues/${id}/register-view`
 }
 export const FETCH_RESTAURANT = (restaurantID: number, recommendationID?: number) => {
-    return `/venues/${restaurantID}${
-        recommendationID !== null && recommendationID !== undefined ? `?recommendationId=${recommendationID}` : ''
-    }`
+    return `/venues/${restaurantID}${recommendationID !== null && recommendationID !== undefined ? `?recommendationId=${recommendationID}` : ''
+        }`
 }
 export const PAGINATE_RECOMMENDATIONS = (restaurantID: number, page: number) => {
     return `/venues/${restaurantID}/recommendations?page=${page}&size=3&sort=createdAt,DESC`
