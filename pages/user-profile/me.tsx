eb7aff536086e94a4527da79cc71d8104fa3015f
Eric Hendrickson <ehendrickson@seamgen.com>
2020-11-20 23:45:24 -0800

    * Added useEffect for getting authorization code

--- pages/user-profile/me.tsx
+++ pages/user-profile/me.tsx
@@ -1,6 +1,9 @@
 import UserProfile from 'components/UserProfile/UserProfile'
+import { INSTAGRAM_CLIENT_ID, INSTAGRAM_CLIENT_SECRET, INSTAGRAM_REDIRECT_URI } from 'constants/InstagramConstants'
 import { KeycloakInstance } from 'keycloak-js'
 import { GetServerSideProps } from 'next'
+import { useRouter } from 'next/router'
+import qs from 'querystring'
 import React from 'react'
 import { connect as reduxConnect } from 'react-redux'
 import { bindActionCreators } from 'redux'
@@ -17,6 +20,25 @@ interface IServerSideProps {
 interface IUserProfileProps extends IServerSideProps, IWithAuthInjectedProps { }
 
 const UserProfileMePage: React.FC<IUserProfileProps> = ({ user, venueListMetaId }) => {
+    const router = useRouter()
+
+    React.useEffect(() => {
+        if (router?.asPath && !(user.instagramId && user.instagramToken)) {
+            const parseAsPath = qs.parse(router.asPath)
+            if (parseAsPath['/?code']) {
+                const authorizationCode: string = (parseAsPath['/?code'] as string).replace('#_', '')
+                console.log(authorizationCode)
+
+                const formData = new FormData()
+                formData.append('client_id', INSTAGRAM_CLIENT_ID)
+                formData.append('client_secret', INSTAGRAM_CLIENT_SECRET)
+                formData.append('grant_type', 'authorization_code')
+                formData.append('code', authorizationCode)
+                formData.append('redirect_uri', INSTAGRAM_REDIRECT_URI)
+            }
+        }
+    }, [user])
+
     return (
         <>
             {user ? (<UserProfile fetchedUser={user} venueListMetaId={venueListMetaId}></UserProfile>) : null}
@@ -26,7 +48,6 @@ const UserProfileMePage: React.FC<IUserProfileProps> = ({ user, venueListMetaId 
 
 export const getServerSideProps: GetServerSideProps = async (context) => {
     const venueListMetaId = context.query.v
-    console.log('there', venueListMetaId)
     return {
         props: {
             venueListMetaId: venueListMetaId !== undefined && venueListMetaId !== null ? Number(venueListMetaId) : null,
