4903f47a72d8b74a134fc311decb683387d2ca5b
boraerbas <bora.erbas@gmail.com>
2021-01-17 10:46:07 +0300

    build error temp fix

--- src/stories/SearchFull.stories.tsx
+++ src/stories/SearchFull.stories.tsx
@@ -3,36 +3,36 @@ import SearchFull from 'components/SearchFull/SearchFull'
 import React from 'react'
 
 export const SearchFullData = {
-    default: {
-        place: 'Sushi',
-        address: '1234 Where St., San Diego, CA',
-    },
+  default: {
+    place: 'Sushi',
+    address: '1234 Where St., San Diego, CA',
+  },
 }
 
-const handleSearch = (place: string, address: string) => {}
+const handleSearch = (place: string, address: string) => { }
 
 storiesOf('Full Search', module)
-    .add('Default', () => {
-        return (
-            <SearchFull
-                inputPlace={null}
-                inputAddress={null}
-                inputLat={null}
-                inputLng={null}
-                handleSearch={() => {}}
-                inputCategoryID={null}
-            />
-        )
-    })
-    .add('With Inputs', () => {
-        return (
-            <SearchFull
-                inputPlace={"Earl's Sandwich"}
-                inputAddress={'San Diego, CA, US'}
-                inputLat={'32.7157'}
-                inputLng={'-117.161'}
-                handleSearch={() => {}}
-                inputCategoryID={null}
-            />
-        )
-    })
+  .add('Default', () => {
+    return (
+      <SearchFull
+        inputPlace={null}
+        inputAddress={null}
+        inputLat={null}
+        inputLng={null}
+        handleSearch={() => { }}
+        inputCategoryID={null}
+      />
+    )
+  })
+  .add('With Inputs', () => {
+    return (
+      <SearchFull
+        inputPlace={"Earl's Sandwich"}
+        inputAddress={'San Diego, CA, US'}
+        inputLat={'32.7157'}
+        inputLng={'-117.161'}
+        handleSearch={() => { }}
+        inputCategoryID={null}
+      />
+    )
+  })
--- src/stories/SearchPage.stories.tsx
+++ src/stories/SearchPage.stories.tsx
@@ -3,27 +3,27 @@ import SearchWorkBench from 'components/SearchWorkBench/SearchWorkBench'
 import React from 'react'
 
 export const SearchFullData = {
-    default: {
-        place: 'Sushi',
-        address: '1234 Where St., San Diego, CA',
-    },
+  default: {
+    place: 'Sushi',
+    address: '1234 Where St., San Diego, CA',
+  },
 }
 
-const handleSearch = (place?: string, address?: string) => {}
+const handleSearch = (place?: string, address?: string) => { }
 
 storiesOf('Search Page', module).add('Search Panel', () => {
-    return (
-        <SearchWorkBench
-            inputPlace={null}
-            inputAddress={null}
-            inputLat={null}
-            inputLng={null}
-            inputSort={null}
-            searchResults={[]}
-            topCategories={[]}
-            handleSearch={() => {}}
-            openSearchModal={() => {}}
-            inputCategoryID={null}
-        />
-    )
+  return (
+    <SearchWorkBench
+      inputPlace={null}
+      inputAddress={null}
+      inputLat={null}
+      inputLng={null}
+      inputSort={null}
+      searchResults={[]}
+      topCategories={[]}
+      handleSearch={() => { }}
+      openSearchModal={() => { }}
+      inputCategoryID={null}
+    />
+  )
 })
