e26d1ffeb61d3bc9dc36ab40aad9612eeb688598
boraerbas <bora.erbas@gmail.com>
2021-02-18 19:33:46 +0300

    fix more indentation

--- src/stories/SearchFull.stories.tsx
+++ src/stories/SearchFull.stories.tsx
@@ -3,13 +3,13 @@ import SearchFull from 'components/SearchFull/SearchFull'
 import React from 'react'
 
 export const SearchFullData = {
-  default: {
-    place: 'Sushi',
-    address: '1234 Where St., San Diego, CA',
-  },
+    default: {
+      place: 'Sushi',
+      address: '1234 Where St., San Diego, CA',
+    },
 }
 
-const handleSearch = (place: string, address: string) => { }
+const handleSearch = (place: string, address: string) => {}
 
 storiesOf('Full Search', module)
     .add('Default', () => {
