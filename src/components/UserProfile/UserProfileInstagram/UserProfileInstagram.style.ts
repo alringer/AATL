6bf24d3b7811ed6edc0497dd0df10d7374fe8c1e
Yoon Chang <ychang@seamgen.com>
2021-03-25 23:27:39 -0700

    - Remove build error

--- src/components/UserProfile/UserProfileInstagram/UserProfileInstagram.style.ts
+++ src/components/UserProfile/UserProfileInstagram/UserProfileInstagram.style.ts
@@ -67,6 +67,19 @@ export const UserProfileInstagramSubTitle = styled.p`
     }
 `
 
+export const UserProfileInstagramPhotosContainer = styled.div`
+    display: flex;
+    width: 100%;
+
+    @media ${device.tablet} {
+        height: 460px;
+    }
+
+    @media ${device.laptop} {
+        height: 460px;
+    }
+`
+
 export const UserProfileInstagramEmptyContainer = styled.div`
     display: flex;
     width: 100%;
