98a6770de9b2134b42c5781178b4de9ef4163009
Yoon Chang <ychang@seamgen.com>
2022-06-22 12:22:12 -0500

    - Add attribute to storybook component to resolve build error

--- src/stories/CardRecommendationWide.stories.tsx
+++ src/stories/CardRecommendationWide.stories.tsx
@@ -1,7 +1,8 @@
 import { storiesOf } from '@storybook/react'
 import PlaceImage from 'assets/mock-images/restaurant_image.jpg'
-import CardRecommendationWide, { CardRecommendationWideEnum } from 'components/CardRecommendationWide/CardRecommendationWide'
-import React from 'react'
+import CardRecommendationWide, {
+    CardRecommendationWideEnum,
+} from 'components/CardRecommendationWide/CardRecommendationWide'
 import { flaggedEnum } from 'utilities/types/enumerations'
 import { IRecommendation } from 'utilities/types/recommendation'
 import { mockUser } from 'utilities/types/userProfile'
@@ -11,8 +12,7 @@ const recommendationDefault: IRecommendation = {
     id: 12,
     imageCDNUrl: PlaceImage,
     venue: mockVenue,
-    title:
-        'An amazingly rich and diverse range of edible delights. An amazingly rich and diverse range of edible delights',
+    title: 'An amazingly rich and diverse range of edible delights. An amazingly rich and diverse range of edible delights',
     content:
         'The only moment, the only life we have is in the NOW. The only moment, the only life we have is in the NOW The only moment, the only life we have is in the NOW The only moment, the only life we have is in the NOW ',
     createdAt: '',
@@ -20,29 +20,31 @@ const recommendationDefault: IRecommendation = {
     deletedAt: '',
     flagged: flaggedEnum.None,
     updatedAt: '',
+    rating: 4,
 }
 
 const recommendationLongTitle: IRecommendation = {
     ...recommendationDefault,
-    title: 'An amazingly rich and diverse range of edible delights. An amazingly rich and diverse range of edible delights. An amazingly rich and diverse range of edible delights. An amazingly rich and diverse range of edible delights. An amazingly rich and diverse range of edible delights. An amazingly rich and diverse range of edible delights.'
+    title: 'An amazingly rich and diverse range of edible delights. An amazingly rich and diverse range of edible delights. An amazingly rich and diverse range of edible delights. An amazingly rich and diverse range of edible delights. An amazingly rich and diverse range of edible delights. An amazingly rich and diverse range of edible delights.',
 }
 
 const recommendationLongDescription: IRecommendation = {
     ...recommendationDefault,
-    content: 'The only moment, the only life we have is in the NOW. The only moment, the only life we have is in the NOW The only moment, the only life we have is in the NOW The only moment, the only life we have is in the NOW The only moment, the only life we have is in the NOW. The only moment, the only life we have is in the NOW The only moment, the only life we have is in the NOW The only moment, the only life we have is in the NOW The only moment, the only life we have is in the NOW. The only moment, the only life we have is in the NOW The only moment, the only life we have is in the NOW The only moment, the only life we have is in the NOW The only moment, the only life we have is in the NOW. The only moment, the only life we have is in the NOW The only moment, the only life we have is in the NOW The only moment, the only life we have is in the NOW'
+    content:
+        'The only moment, the only life we have is in the NOW. The only moment, the only life we have is in the NOW The only moment, the only life we have is in the NOW The only moment, the only life we have is in the NOW The only moment, the only life we have is in the NOW. The only moment, the only life we have is in the NOW The only moment, the only life we have is in the NOW The only moment, the only life we have is in the NOW The only moment, the only life we have is in the NOW. The only moment, the only life we have is in the NOW The only moment, the only life we have is in the NOW The only moment, the only life we have is in the NOW The only moment, the only life we have is in the NOW. The only moment, the only life we have is in the NOW The only moment, the only life we have is in the NOW The only moment, the only life we have is in the NOW',
 }
 
 const recommendationLongTitleAndLongDescription: IRecommendation = {
     ...recommendationDefault,
     title: recommendationLongTitle.title,
-    content: recommendationLongDescription.content
+    content: recommendationLongDescription.content,
 }
 
 export const RecommendationCardData = {
     default: recommendationDefault,
     longTitle: recommendationLongTitle,
     longDescription: recommendationLongDescription,
-    longTitleAndDescription: recommendationLongTitleAndLongDescription
+    longTitleAndDescription: recommendationLongTitleAndLongDescription,
 }
 
 export const RecommendationCardsListData = {
@@ -70,26 +72,82 @@ storiesOf('Card: Wide Recommendation Card', module)
         return <CardRecommendationWide isFull={true} recommendation={RecommendationCardData.longTitleAndDescription} />
     })
     .add('Profile - Full Card', () => {
-        return <div style={{ maxWidth: '690px', width: '100%' }}><CardRecommendationWide type={CardRecommendationWideEnum.Profile} isFull={false} recommendation={RecommendationCardData.default} /></div>
+        return (
+            <div style={{ maxWidth: '690px', width: '100%' }}>
+                <CardRecommendationWide
+                    type={CardRecommendationWideEnum.Profile}
+                    isFull={false}
+                    recommendation={RecommendationCardData.default}
+                />
+            </div>
+        )
     })
     .add('Profile - Long Title Full Card', () => {
-        return <div style={{ maxWidth: '690px', width: '100%' }}><CardRecommendationWide type={CardRecommendationWideEnum.Profile} isFull={false} recommendation={RecommendationCardData.longTitle} /></div>
+        return (
+            <div style={{ maxWidth: '690px', width: '100%' }}>
+                <CardRecommendationWide
+                    type={CardRecommendationWideEnum.Profile}
+                    isFull={false}
+                    recommendation={RecommendationCardData.longTitle}
+                />
+            </div>
+        )
     })
     .add('Profile - Long Description Full Card', () => {
-        return <div style={{ maxWidth: '690px', width: '100%' }}><CardRecommendationWide type={CardRecommendationWideEnum.Profile} isFull={false} recommendation={RecommendationCardData.longDescription} /></div>
+        return (
+            <div style={{ maxWidth: '690px', width: '100%' }}>
+                <CardRecommendationWide
+                    type={CardRecommendationWideEnum.Profile}
+                    isFull={false}
+                    recommendation={RecommendationCardData.longDescription}
+                />
+            </div>
+        )
     })
     .add('Profile - Long Title and Description Full Card', () => {
-        return <div style={{ maxWidth: '690px', width: '100%' }}><CardRecommendationWide type={CardRecommendationWideEnum.Profile} isFull={false} recommendation={RecommendationCardData.longTitleAndDescription} /></div>
+        return (
+            <div style={{ maxWidth: '690px', width: '100%' }}>
+                <CardRecommendationWide
+                    type={CardRecommendationWideEnum.Profile}
+                    isFull={false}
+                    recommendation={RecommendationCardData.longTitleAndDescription}
+                />
+            </div>
+        )
     })
     .add('Venue Page - Full Card', () => {
-        return <CardRecommendationWide isFull={true} recommendation={RecommendationCardData.default} type={CardRecommendationWideEnum.Restaurant} />
+        return (
+            <CardRecommendationWide
+                isFull={true}
+                recommendation={RecommendationCardData.default}
+                type={CardRecommendationWideEnum.Restaurant}
+            />
+        )
     })
     .add('Venue Page - Long Title Full Card', () => {
-        return <CardRecommendationWide isFull={true} recommendation={RecommendationCardData.longTitle} type={CardRecommendationWideEnum.Restaurant} />
+        return (
+            <CardRecommendationWide
+                isFull={true}
+                recommendation={RecommendationCardData.longTitle}
+                type={CardRecommendationWideEnum.Restaurant}
+            />
+        )
     })
     .add('Venue Page - Long Description Full Card', () => {
-        return <CardRecommendationWide isFull={true} recommendation={RecommendationCardData.longDescription} type={CardRecommendationWideEnum.Restaurant} />
+        return (
+            <CardRecommendationWide
+                isFull={true}
+                recommendation={RecommendationCardData.longDescription}
+                type={CardRecommendationWideEnum.Restaurant}
+            />
+        )
     })
     .add('Venue Page - Long Title and Description Full Card', () => {
-        return <CardRecommendationWide isFull={true} recommendation={RecommendationCardData.longTitleAndDescription} type={CardRecommendationWideEnum.Restaurant} />
+        return (
+            <CardRecommendationWide
+                isFull={true}
+                recommendation={RecommendationCardData.longTitleAndDescription}
+                type={CardRecommendationWideEnum.Restaurant}
+            />
+        )
     })
